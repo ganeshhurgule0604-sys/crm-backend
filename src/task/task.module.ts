16090d504a22f32d2c7dd0a147124a019ace018a
Ganesh Hurgule <ganesh.hurgule@indexnine.com>
2026-05-14 22:16:43 +0530

    fixed task error

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { TaskRepository } from './task.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './task.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Task])],
   controllers: [TaskController],
-  providers: [TaskService]
+  providers: [TaskService,TaskRepository],
 })
 export class TaskModule {}
