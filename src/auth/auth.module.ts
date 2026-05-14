6c8420aa3a62ae61ab1a02d49e9b61ea589140b2
Ganesh Hurgule <ganesh.hurgule@indexnine.com>
2026-05-14 23:42:16 +0530

    fixed jwt token issue

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,17 +2,30 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
   imports: [
-    JwtModule.register({
-      secret: 'learn',
-      signOptions: { expiresIn: '1d' },
+
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET')!,
+
+        signOptions: {
+          expiresIn: '1d',
+        },
+      }),
     }),
+
     UserModule,
   ],
+
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { payloadDto } from './auth.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(configService:ConfigService) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     super({
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // ✅ typed correctly
       ignoreExpiration: false,
-      secretOrKey: 'learn',
+      secretOrKey:configService.get<string>('JWT_SECRET') || 'defaultSecret',
     });
   }
 
